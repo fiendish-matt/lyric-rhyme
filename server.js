8845e1ed8d1813aa2ec64824b6d5318b0f8618f5
Matty <mattfiend007@proton.me>
2023-08-21 03:23:55 +0530

    Chnges dded

--- server.js
+++ server.js
@@ -1,104 +1,45 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const authRoutes = require("./route");
-
-
-require("dotenv").config();
-
-
+const express = require('express');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const app = express();
+const port = process.env.PORT || 3000;
 
-const router = express.Router();
+// Define a route for the cloaked affiliate links
+app.get('/cloaked', async (req, res) => {
+  const originalUrl = 'https://redirecting7.eu/p/GebT/0Fj3/et6K/'; // Replace with your affiliate link
+  const cloakedUrl = modifyAffiliateLink(originalUrl);
 
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(cors({ origin: process.env.CLIENT_URL }));
+  try {
+    // Create a virtual DOM using jsdom
+    const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
+    const window = dom.window;
+    const document = window.document;
 
-app.use("/api",router);
+    // Add a script to perform the redirection
+    const script = document.createElement('script');
+    script.textContent = `window.location.replace("${encodeURI(cloakedUrl)}");`;
+    document.body.appendChild(script);
 
-const targetUrls = {
-  type25: "https://ad.admitad.com/g/cbvmqmmirj56bc9a2c7845f1c03a4a/",
-  splinter: "https://redirecting7.eu/p/GebT/0Fj3/et6K",
-};
+    // Serialize the modified DOM
+    const modifiedHtml = dom.serialize();
 
-app.use((req, res, next) => {
-  if (req.query.id === undefined && req.query.info === undefined) {
-    res.sendStatus(400);
-    return;
-  }
-  const targetUrl = targetUrls[req.query.id];
-  const targetUrlv2 = targetUrls[req.query.info];
-
-  let response;
-
-  if (targetUrl || targetUrlv2) {
-    try {
-      response = fetch(targetUrl || targetUrlv2, {
-        method: "GET",
-      });
-      response.then((resp) => {
-        if (resp.url) {
-          createProxyMiddleware({
-            target: resp.url,
-            changeOrigin: true,
-            selfHandleResponse: true,
-            onProxyRes: async (proxyRes, req, res) => {
-              const finalUrl = proxyRes.headers["location"] || resp.url;
-
-              if(targetUrl){
-                  res.set("Location", finalUrl);
-                  res.setHeader("Location", finalUrl);
-                  res.setHeader("Cache-Control", "no-store"); // Prevent caching
-                  res.setHeader("X-Frame-Options", "DENY");    // Set X-Frame-Options      
-                  res.sendStatus(302).end(); // Send a redirect response
-              }
-              else if(targetUrlv2){
-                const htmlResponse = `
-                <!DOCTYPE html>
-                <html>
-                <head>
-                <title>resource loading</title>
-                </head>
-                <body>
-                 <a id="dynamic-link" href="${finalUrl}"></a>
-                <script>
-                    document.addEventListener('DOMContentLoaded', function() {
-                    window.setTimeout(()=>{
-                      window.location.assign("${finalUrl}")
-                    },1000)
-                    });
-                </script>
-                </body>
-                </html>
-              `; 
-              res.send(htmlResponse);
-              }
-              return;
-            },
-          })(req, res, next);
-        } else {
-          next();
-        }
-      });
-    } catch (error) {
-      console.log("error", error);
-      res.sendStatus(400);
-    }
-  } else {
-    return res.status(400).json({
-      error: "invalid url",
-    });
+    // Send the modified HTML as the response
+    res.send(modifiedHtml);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred.');
   }
 });
 
+// Modify the affiliate link as needed
+function modifyAffiliateLink(originalUrl) {
+  // Example: Adding an affiliate tracking parameter
+  const affiliateTrackingCode = 'your_tracking_code';
+  const modifiedUrl = `${originalUrl}?affiliate=${affiliateTrackingCode}`;
+  return modifiedUrl;
+}
 
-
-const port = process.env.PORT;
-
+// Start the server
 app.listen(port, () => {
-  console.log(`running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
